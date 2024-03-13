4676c6c4af99c0cfd2bc47b6934a35f48e6d652a
ng <ng_zi_jie@hotmail.com>
2024-03-13 12:13:39 +0800

    css halfway done

--- app/javascript/controllers/mygarden_controller.js
+++ app/javascript/controllers/mygarden_controller.js
@@ -10,15 +10,19 @@ export default class extends Controller {
   }
 
   toggleCollection() {
-    this.collectionHeaderTarget.classList.remove("disabled");
-    this.plantsHeaderTarget.classList.add("disabled");
+    this.collectionHeaderTarget.classList.remove("clickable");
+    this.collectionHeaderTarget.classList.add("index_header_toggle");
+    this.plantsHeaderTarget.classList.add("clickable");
+    this.plantsHeaderTarget.classList.remove("index_header_toggle");
     this.collectionTarget.classList.remove("d-none")
     this.plantsTarget.classList.add("d-none");
   }
 
   togglePlant() {
-    this.collectionHeaderTarget.classList.add("disabled");
-    this.plantsHeaderTarget.classList.remove("disabled");
+    this.collectionHeaderTarget.classList.add("clickable");
+    this.collectionHeaderTarget.classList.remove("index_header_toggle");
+    this.plantsHeaderTarget.classList.remove("clickable");
+    this.plantsHeaderTarget.classList.add("index_header_toggle");
     this.collectionTarget.classList.add("d-none")
     this.plantsTarget.classList.remove("d-none");
   }
