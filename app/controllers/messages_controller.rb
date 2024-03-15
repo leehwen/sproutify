52321c7515ab21c5df345fe27b46fee5b24a1333
YY T <changiyy07@gmail.com>
2024-03-15 17:35:14 +0800

    change chat msg styling

--- app/controllers/messages_controller.rb
+++ app/controllers/messages_controller.rb
@@ -8,7 +8,8 @@ class MessagesController < ApplicationController
     if @message.save
       OfferChannel.broadcast_to(
         @offer,
-        render_to_string(partial: "message", locals: { message: @message })
+        sender: current_user.id,
+        message: render_to_string(partial: "message", locals: { message: @message })
       )
       head :ok
     else
